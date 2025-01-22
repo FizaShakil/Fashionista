b2bb827ac058ed2dee16f88746483b24af5858a8
FizaShakil <fizashakil25@gmail.com>
2025-01-22 22:45:57 +0500

    Added categorry model

---
+++ server/src/models/category.model.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from 'mongoose'
+
+const categorySchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    productId:[ //array of product iems which lay in the category
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Product"
+        }
+    ],
+    imageLink:{
+        type: String,
+        required: true
+    }
+})
