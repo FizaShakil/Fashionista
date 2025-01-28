7e86f9ba54147adab9488ad80ba008331dd82047
FizaShakil <fizashakil25@gmail.com>
2025-01-28 22:59:08 +0500

    adding the registerUser route

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors'
 import cookieParser from'cookie-parser'
+import userRouter from './routes/user.router.js'
 
 
 const app = express();
@@ -20,5 +21,7 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+app.use("/api/v1/users" , userRouter)
 
 export default app;
