435d0a15e295f447334749f53cd8ae5c4be08ade
FizaShakil <fizashakil25@gmail.com>
2025-02-22 00:02:14 +0500

    Added more controllers for product

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -1,5 +1,4 @@
 import { Product } from "../models/product.model.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
@@ -9,7 +8,7 @@ const uploadProductDetails = asyncHandler(async(req,res)=>{
     const {name, description, price, gender, newArrival} = req.body;
     console.log("Name: ", name)
 
-    if([name, description, price, gender, newArrival].some((field)=>field?.trim === " ")){
+    if([name, productUniqueID, description, price, gender, newArrival].some((field)=>field?.trim === " ")){
         throw new ApiError(400, "All fields are required")
     }
 
@@ -31,10 +30,11 @@ const uploadProductDetails = asyncHandler(async(req,res)=>{
     // if everything is working fine, create entry in DB
     const product = await Product.create({
         name, 
+        productUniqueID,
         description, 
         price, 
         gender,
-        imageLink: productImage.url, 
+        productImage: productImage.url, 
         newArrival
     })
 
@@ -52,4 +52,85 @@ const uploadProductDetails = asyncHandler(async(req,res)=>{
     )
 })
 
-export {uploadProductDetails}
+//update product details
+const updateProductDetails = asyncHandler(async(req,res)=>{
+    const {name, description, price, gender, newArrival} = req.body;   
+    
+    const product = await Product.findByIdAndUpdate(
+        req.product?._id,
+        {
+            $set:{
+                name, 
+                description, 
+                price, 
+                gender, 
+                newArrival
+            }
+        },
+        {new: true}
+    )
+    // If product not found, throw error
+    if(!product){
+        throw new ApiError(400, "Product not found!")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, product, "User details updated successfully!! ")
+    )
+})
+
+//delete product details
+const deleteProductDetails = asyncHandler(async(req,res)=>{
+
+    //give product unique id to delete
+    const {productUniqueID} = req.body;
+     
+    //if product unique id is not given, throw error
+    if(!productUniqueID){
+        throw new ApiError(400, "Product's unique ID is required to delete")
+    }
+    
+    //delete product
+    const product = await Product.findByIdAndDelete(req.product._id)
+    
+    // if no product exist, throw error
+    if(!product){
+        throw new ApiError(400, "Product ID not found")
+    }
+
+    //return response
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, product, "User account deleted successfully!")
+    )
+})
+
+// Get product details
+const getProductDetails = asyncHandler(async(req,res)=>{
+
+    const {productUniqueID} = req.body;
+     if(!productUniqueID){
+        throw new ApiError(400, "Product Unique ID required to get the product")
+     }
+      const product = Product.findById(req.product?._id)
+
+    // if no product exist, throw error
+    if(!product){
+        throw new ApiError(400, "Product ID not found")
+    }
+
+    //return response
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, product, "User found successfully!")
+    ) 
+})
+export {
+    uploadProductDetails,
+    updateProductDetails,
+    deleteProductDetails,
+    getProductDetails
+}
