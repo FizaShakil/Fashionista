58047c08230309b834aa7e975af170e00d59564a
FizaShakil <fizashakil25@gmail.com>
2025-03-16 11:39:44 +0500

    Make Modifications n the filename generate function and removed unwanted code

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -5,21 +5,12 @@ const storage = multer.diskStorage({
         cb(null, './public/temp')
     },
     filename: function(req, file, cb) {
-        cb(null, file.fieldname)
+        cb(null, Date.now() + "-" + file.originalname)
     }
 })
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only image files are allowed!"), false);
-    }
-  }
 
 export const upload = multer(
     {
         storage: storage,
-        fileFilter: fileFilter
     }
 )
