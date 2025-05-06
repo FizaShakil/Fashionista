88d3e9a4f42efe7fc830a67e9db418c3dd8daf5c
FizaShakil <fizashakil25@gmail.com>
2025-05-06 18:25:17 +0500

    Updated the item fields according to the database fields

--- client/src/Redux/cartSlice.js
+++ client/src/Redux/cartSlice.js
@@ -30,31 +30,31 @@ const cartSlice = createSlice({
     reducers:{
           addToCart: (state, action) => {
           const product = action.payload;
-          const existingProduct = state.items.find(item => item.id === product.id);
+          const existingProduct = state.items.find(item => item._id === product._id);
   
         if (existingProduct) {
-          // Product already in cart ➔ increase quantity
+          // Product already in cart -- increase quantity
           existingProduct.quantity += 1;
         } else {
-          // Product not in cart ➔ add it with quantity: 1
+          // Product not in cart -- add it with quantity: 1
           state.items.push({ ...product, quantity: 1 });
         }
         saveCartToLocalStorage(state.items); 
           },
 
           removeFromCart: (state, action) => {
-            state.items = state.items.filter(item => item.id !== action.payload);
+            state.items = state.items.filter(item => item._id !== action.payload);
             saveCartToLocalStorage(state.items); 
           },
 
           incrementQuantity: (state, action) => {
-            const item = state.items.find(item => item.id === action.payload);
+            const item = state.items.find(item => item._id === action.payload);
             if (item) item.quantity += 1;
             saveCartToLocalStorage(state.items); 
           },
           
           decrementQuantity: (state, action) => {
-            const item = state.items.find(item => item.id === action.payload);
+            const item = state.items.find(item => item._id === action.payload);
             if (item && item.quantity > 1) item.quantity -= 1;
             saveCartToLocalStorage(state.items); 
           },
