53ad6281f23b69c8c76e6b29184ca7ea8083b3d1
FizaShakil <fizashakil25@gmail.com>
2025-07-14 23:53:16 +0500

    Added Order model

---
+++ server/src/models/order.model.js
@@ -0,0 +1,52 @@
+ import mongoose, {Schema} from "mongoose"
+
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const orderSchema = new mongoose.Schema({
+	userID: {
+		type: ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	products: [
+		{
+			productID: {
+				type: ObjectId,
+				ref: 'Product',
+				required: true,
+			}, 
+			quantity: { 
+				type: Number, 
+				default: 1,
+			},
+		},
+	],
+	items: {
+		type: ObjectId,
+		ref: 'Cart',
+	},
+	amount: {
+		type: Number,
+		required: true,
+	},
+	email: {
+		type: Number,
+		required: true,
+	},
+	address: { 
+		type: ObjectId, 
+		required: true,
+	},
+	phone:{
+		type: String,
+		required: true,
+	},
+	status: {
+		type: String,
+		default: "pending",
+	},
+}, 
+	{timestamps: true}
+)
+
+export const Order = mongoose.model("Order", orderSchema)
