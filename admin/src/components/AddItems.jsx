d1b01f6127ccb273dc817e98106f4da13f455576
FizaShakil <fizashakil25@gmail.com>
2025-07-29 23:07:43 +0500

    Fixed error in admin panel

--- admin/src/components/AddItems.jsx
+++ admin/src/components/AddItems.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import {useNavigate} from 'react-router-dom'
-import axiosInstance from "../../../client/src/axiosInstance";
+import axiosInstance from "../axiosInstance";
 
 const AddItems= () =>{
   const navigate = useNavigate()
