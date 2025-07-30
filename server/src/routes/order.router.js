5fa6aa6656dbf2ab15d9722a4276681d9eb9d0b5
FizaShakil <fizashakil25@gmail.com>
2025-07-30 22:10:00 +0500

    Fixed the missing middleware in order router

--- server/src/routes/order.router.js
+++ server/src/routes/order.router.js
@@ -7,7 +7,7 @@ const orderRouter = Router()
 
 orderRouter.route('/place-order').post(verifyJWT, placeUserOrder)
 orderRouter.route('/get-user-orders').get(verifyAnyJWT, getUserOrders)
-orderRouter.route('/get-single-order/:id').get(verifyJWT, getSingleOrder)
+orderRouter.route('/get-single-order/:id').get(verifyAdminJWT,isAdmin, getSingleOrder)
 orderRouter.route('/admin/get-all-orders').get(verifyAdminJWT,isAdmin, getAllOrders)
 orderRouter.route('/update-order-status/:id').patch(verifyAdminJWT,isAdmin, updateOrderStatus)
 
